29af00e683be636b62695ed7be3b789324799629
Angelica Kusik <angelicakusik@outlook.com>
2022-05-01 15:35:07 -0400

    Created repository
    Uploaded the latest version of the Units Shipped Calculator Application.

---
+++ UnitsShipped.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace UnitsShippedCalculator
+{
+    partial class formUnitsShipped
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelUnitsPrompt = new System.Windows.Forms.Label();
+            this.textBoxPastEntriesRegion1 = new System.Windows.Forms.TextBox();
+            this.textBoxUnitsInput = new System.Windows.Forms.TextBox();
+            this.labelGeneralAverageOutput = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.labelRegion1 = new System.Windows.Forms.Label();
+            this.textBoxPastEntriesRegion2 = new System.Windows.Forms.TextBox();
+            this.labelRegion2 = new System.Windows.Forms.Label();
+            this.textBoxPastEntriesRegion3 = new System.Windows.Forms.TextBox();
+            this.labelRegion3 = new System.Windows.Forms.Label();
+            this.labelRegion1Output = new System.Windows.Forms.Label();
+            this.labelRegion2Output = new System.Windows.Forms.Label();
+            this.labelRegion3Output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(63, 699);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(186, 40);
+            this.buttonEnter.TabIndex = 13;
+            this.buttonEnter.Text = "&Enter";
+            this.toolTip1.SetToolTip(this.buttonEnter, "Click to calculate average units shipped.");
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(283, 699);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(186, 40);
+            this.buttonReset.TabIndex = 14;
+            this.buttonReset.Text = "&Reset";
+            this.toolTip1.SetToolTip(this.buttonReset, "Click to reset application.");
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(499, 699);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(186, 40);
+            this.buttonExit.TabIndex = 15;
+            this.buttonExit.Text = "E&xit";
+            this.toolTip1.SetToolTip(this.buttonExit, "Click to exit application.");
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelUnitsPrompt
+            // 
+            this.labelUnitsPrompt.AutoSize = true;
+            this.labelUnitsPrompt.Location = new System.Drawing.Point(63, 33);
+            this.labelUnitsPrompt.Name = "labelUnitsPrompt";
+            this.labelUnitsPrompt.Size = new System.Drawing.Size(71, 30);
+            this.labelUnitsPrompt.TabIndex = 1;
+            this.labelUnitsPrompt.Text = "&Cases:";
+            // 
+            // textBoxPastEntriesRegion1
+            // 
+            this.textBoxPastEntriesRegion1.Location = new System.Drawing.Point(90, 126);
+            this.textBoxPastEntriesRegion1.Multiline = true;
+            this.textBoxPastEntriesRegion1.Name = "textBoxPastEntriesRegion1";
+            this.textBoxPastEntriesRegion1.ReadOnly = true;
+            this.textBoxPastEntriesRegion1.Size = new System.Drawing.Size(166, 384);
+            this.textBoxPastEntriesRegion1.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.textBoxPastEntriesRegion1, "Displays region 1 units shipped entered..");
+            // 
+            // textBoxUnitsInput
+            // 
+            this.textBoxUnitsInput.Location = new System.Drawing.Point(140, 33);
+            this.textBoxUnitsInput.Name = "textBoxUnitsInput";
+            this.textBoxUnitsInput.Size = new System.Drawing.Size(150, 35);
+            this.textBoxUnitsInput.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.textBoxUnitsInput, "Enter today\'s units shipped for each region.");
+            // 
+            // labelGeneralAverageOutput
+            // 
+            this.labelGeneralAverageOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelGeneralAverageOutput.Location = new System.Drawing.Point(63, 616);
+            this.labelGeneralAverageOutput.Name = "labelGeneralAverageOutput";
+            this.labelGeneralAverageOutput.Size = new System.Drawing.Size(622, 45);
+            this.labelGeneralAverageOutput.TabIndex = 12;
+            this.labelGeneralAverageOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelGeneralAverageOutput, "Displays total average units shipped considering all 3 regions.");
+            // 
+            // labelRegion1
+            // 
+            this.labelRegion1.AutoSize = true;
+            this.labelRegion1.Location = new System.Drawing.Point(127, 84);
+            this.labelRegion1.Name = "labelRegion1";
+            this.labelRegion1.Size = new System.Drawing.Size(94, 30);
+            this.labelRegion1.TabIndex = 3;
+            this.labelRegion1.Text = "Region 1";
+            // 
+            // textBoxPastEntriesRegion2
+            // 
+            this.textBoxPastEntriesRegion2.Location = new System.Drawing.Point(294, 126);
+            this.textBoxPastEntriesRegion2.Multiline = true;
+            this.textBoxPastEntriesRegion2.Name = "textBoxPastEntriesRegion2";
+            this.textBoxPastEntriesRegion2.ReadOnly = true;
+            this.textBoxPastEntriesRegion2.Size = new System.Drawing.Size(166, 384);
+            this.textBoxPastEntriesRegion2.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.textBoxPastEntriesRegion2, "Displays region 2 units shipped entered.");
+            // 
+            // labelRegion2
+            // 
+            this.labelRegion2.AutoSize = true;
+            this.labelRegion2.Location = new System.Drawing.Point(331, 84);
+            this.labelRegion2.Name = "labelRegion2";
+            this.labelRegion2.Size = new System.Drawing.Size(94, 30);
+            this.labelRegion2.TabIndex = 4;
+            this.labelRegion2.Text = "Region 2";
+            // 
+            // textBoxPastEntriesRegion3
+            // 
+            this.textBoxPastEntriesRegion3.Location = new System.Drawing.Point(498, 126);
+            this.textBoxPastEntriesRegion3.Multiline = true;
+            this.textBoxPastEntriesRegion3.Name = "textBoxPastEntriesRegion3";
+            this.textBoxPastEntriesRegion3.ReadOnly = true;
+            this.textBoxPastEntriesRegion3.Size = new System.Drawing.Size(166, 384);
+            this.textBoxPastEntriesRegion3.TabIndex = 8;
+            this.toolTip1.SetToolTip(this.textBoxPastEntriesRegion3, "Displays region 3 units shipped entered.");
+            // 
+            // labelRegion3
+            // 
+            this.labelRegion3.AutoSize = true;
+            this.labelRegion3.Location = new System.Drawing.Point(535, 84);
+            this.labelRegion3.Name = "labelRegion3";
+            this.labelRegion3.Size = new System.Drawing.Size(94, 30);
+            this.labelRegion3.TabIndex = 5;
+            this.labelRegion3.Text = "Region 3";
+            // 
+            // labelRegion1Output
+            // 
+            this.labelRegion1Output.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelRegion1Output.Location = new System.Drawing.Point(63, 551);
+            this.labelRegion1Output.Name = "labelRegion1Output";
+            this.labelRegion1Output.Size = new System.Drawing.Size(205, 45);
+            this.labelRegion1Output.TabIndex = 9;
+            this.labelRegion1Output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelRegion1Output, "Displays region 1 average units shipped.");
+            // 
+            // labelRegion2Output
+            // 
+            this.labelRegion2Output.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelRegion2Output.Location = new System.Drawing.Point(274, 551);
+            this.labelRegion2Output.Name = "labelRegion2Output";
+            this.labelRegion2Output.Size = new System.Drawing.Size(205, 45);
+            this.labelRegion2Output.TabIndex = 10;
+            this.labelRegion2Output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelRegion2Output, "Displays region 2 average units shipped.");
+            // 
+            // labelRegion3Output
+            // 
+            this.labelRegion3Output.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelRegion3Output.Location = new System.Drawing.Point(485, 551);
+            this.labelRegion3Output.Name = "labelRegion3Output";
+            this.labelRegion3Output.Size = new System.Drawing.Size(200, 45);
+            this.labelRegion3Output.TabIndex = 11;
+            this.labelRegion3Output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelRegion3Output, "Displays region 3 average units shipped.");
+            // 
+            // formUnitsShipped
+            // 
+            this.AcceptButton = this.buttonEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CancelButton = this.buttonReset;
+            this.ClientSize = new System.Drawing.Size(753, 765);
+            this.Controls.Add(this.labelRegion3Output);
+            this.Controls.Add(this.labelRegion2Output);
+            this.Controls.Add(this.labelRegion1Output);
+            this.Controls.Add(this.labelRegion3);
+            this.Controls.Add(this.textBoxPastEntriesRegion3);
+            this.Controls.Add(this.labelRegion2);
+            this.Controls.Add(this.textBoxPastEntriesRegion2);
+            this.Controls.Add(this.labelRegion1);
+            this.Controls.Add(this.labelGeneralAverageOutput);
+            this.Controls.Add(this.textBoxUnitsInput);
+            this.Controls.Add(this.textBoxPastEntriesRegion1);
+            this.Controls.Add(this.labelUnitsPrompt);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonEnter);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formUnitsShipped";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Average Units Shipped by Region";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelUnitsPrompt;
+        private System.Windows.Forms.TextBox textBoxPastEntriesRegion1;
+        private System.Windows.Forms.TextBox textBoxUnitsInput;
+        private System.Windows.Forms.Label labelGeneralAverageOutput;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label labelRegion1;
+        private System.Windows.Forms.TextBox textBoxPastEntriesRegion2;
+        private System.Windows.Forms.Label labelRegion2;
+        private System.Windows.Forms.TextBox textBoxPastEntriesRegion3;
+        private System.Windows.Forms.Label labelRegion3;
+        private System.Windows.Forms.Label labelRegion1Output;
+        private System.Windows.Forms.Label labelRegion2Output;
+        private System.Windows.Forms.Label labelRegion3Output;
+    }
+}
+
---
+++ UnitsShipped.cs
@@ -0,0 +1,242 @@
+﻿/* 
+ 
+Author: Angelica Kuik #100849912
+ Date: February 22, 2022
+ Course: .NET
+ Application: UnitsShippedCalculator - VERSION 2
+ Description: UnitsShippedCalculator VERSION 2 is a Windows Forms application that enables the user to
+              enter the number of units shipped each day of the week for 3 different regions, validates the user input, 
+              calculates the average units per region and the overall average units shipped for all three regions.
+
+              UnitsShippedCalculator VERSION 2 is an updated version of the application created for Lab 1 that has gained
+              two more regions and incorporated the use of 2 dimensional arrays following the requirements of Lab 3.
+ 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnitsShippedCalculator
+{
+    public partial class formUnitsShipped : Form
+    {
+        //Variable Declarations:
+        int rowIndexArray = 0;
+        int columnIndexArray = 0;
+
+
+        const int maxUnitsShipped = 5000;
+        const int minUnitsShipped = 0;
+        const int numberRows = 3;
+        const int numberDays = 7;
+        double averageShippedUnits = 0.0;
+        int sum = 0;
+
+        //Create a two dimensional array to hold the units shipped from each region.
+        int[,] unitsShippedArray = new int[numberRows, numberDays];
+
+        //Create an array of controls for the regions and for the average labels
+        TextBox[] outputTextboxesArray;
+        Label[] outputRegionAverageArray;
+
+        //Create an array to hold the average from each region
+        double[] averageRegionArray = new double[numberRows];
+
+        public formUnitsShipped()
+        {
+            InitializeComponent();
+
+            outputTextboxesArray = new TextBox[] { textBoxPastEntriesRegion1, textBoxPastEntriesRegion2, textBoxPastEntriesRegion3 };
+
+            outputRegionAverageArray = new Label[] { labelRegion1Output, labelRegion2Output, labelRegion3Output };
+
+        }
+        #region Functions
+        /// <summary>
+        /// Given an array of controls, this function clears their text.
+        /// </summary>
+        /// <param name="controlArray">An array of controls with a text property to clear</param>
+        private void ClearControls(Control[] controlArray)
+        {
+            foreach (Control controlToClear in controlArray)
+            {
+                controlToClear.Text = String.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Clears all controls and sets form to its default state.
+        /// </summary>
+        public void SetDefaults()
+        {
+            // Clear all input & output fields:
+            ClearControls(outputRegionAverageArray);
+            ClearControls(outputTextboxesArray);
+            labelGeneralAverageOutput.Text = String.Empty;
+
+            // Reset focus to the input textbox:
+            textBoxUnitsInput.Focus();
+
+            // Reset the variable that holds the average of shipped units:
+            averageShippedUnits = 0.0;
+
+            // Reset the variable that holds the count of each input entered by the user:
+            rowIndexArray = 0;
+            columnIndexArray = 0;
+
+            // Reset the variable that holds the sum of inputs:
+            sum = 0;
+
+            // Reset buttonEnter to enable user to enter new set of inputs:
+            buttonEnter.Enabled = true;
+
+            // Disable textBoxUnitsInput from ReadOnly mode:
+            textBoxUnitsInput.ReadOnly = false;
+
+            //Set labels background color to default
+            foreach (Label label in outputRegionAverageArray)
+            {
+                label.BackColor = Color.LightGray;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Closes the application.
+        /// </summary>
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        /// <summary>
+        /// Clears the input and output fields and resets form to its default state.
+        /// </summary>
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            SetDefaults();
+        }
+        /// <summary>
+        /// Accepts inputs, calculate average and display output to the user.
+        /// </summary>
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            //Local Variable Declarations
+            int shippedUnitsInput;
+
+
+            //Validate user input:
+            if (int.TryParse(textBoxUnitsInput.Text, out shippedUnitsInput))
+            {
+                // Validate if user input is within range:
+                if (shippedUnitsInput >= minUnitsShipped && shippedUnitsInput <= maxUnitsShipped)
+                {
+                    unitsShippedArray[rowIndexArray, columnIndexArray] = shippedUnitsInput;
+                    outputTextboxesArray[rowIndexArray].Text += shippedUnitsInput + Environment.NewLine;
+
+
+                    sum += unitsShippedArray[rowIndexArray, columnIndexArray];
+
+                    columnIndexArray++;
+
+
+                    // Clear textBoxUnitInput after each entry:
+                    textBoxUnitsInput.Clear();
+
+                    labelGeneralAverageOutput.Text = String.Empty;
+
+
+                    // Enables user to enter inputs for all 7 days of the week:
+                    if (columnIndexArray == unitsShippedArray.GetLength(1))
+                    {
+                        // Calculate and display region average 
+                        averageShippedUnits = (double)sum / unitsShippedArray.GetLength(1);
+
+                        // Display average shipped units for region the region.
+                        outputRegionAverageArray[rowIndexArray].Text = string.Format("Average: {0}", Math.Round(averageShippedUnits, 2, MidpointRounding.ToEven));
+
+                        //Add value to the array with all region averages
+                        averageRegionArray[rowIndexArray] = averageShippedUnits;
+
+                        //Increment row index
+                        rowIndexArray++;
+
+                        //Reset column index 
+                        columnIndexArray = 0;
+
+                        //Reset sum
+                        sum = 0;
+
+                        if (rowIndexArray == unitsShippedArray.GetLength(0))
+                        {
+                            //Calculate overall average
+                            for (int row = 0; row < numberRows; row++)
+                            {
+                                for (int column = 0; column < numberDays; column++)
+                                {
+                                    sum += unitsShippedArray[row, column];
+                                }
+                            }
+
+                            averageShippedUnits = sum / unitsShippedArray.Length;
+
+                            // Display overall average shipped units for all 3 regions.
+                            labelGeneralAverageOutput.Text = string.Format("Average: {0}", Math.Round(averageShippedUnits, 2, MidpointRounding.ToEven));
+
+
+                            // Disable buttonEnter after inputs for all 7 days were entered.
+                            buttonEnter.Enabled = false;
+
+                            // Set input textbox to read only so the user can't enter new inputs:
+                            textBoxUnitsInput.ReadOnly = true;
+
+                            //Set backgroung color of region average output to red if < than overall average.
+                            for (int i = 0; i < numberRows; i++)
+                            {
+                                if (averageRegionArray[i] < averageShippedUnits)
+                                {
+                                    outputRegionAverageArray[i].BackColor = Color.Red;
+                                }
+
+                                if (averageRegionArray[i] > averageShippedUnits)
+                                {
+                                    outputRegionAverageArray[i].BackColor = Color.LightGreen;
+                                }
+                            }
+                         
+                        }
+
+                    }
+                }
+                else
+                {
+                    // If range validation fails display the error message below:
+                    labelGeneralAverageOutput.Text = "Please enter a number between " + minUnitsShipped + " and " + maxUnitsShipped;
+
+                    // If validation fails reset focus on the input textbox so user can enter new input. 
+                    textBoxUnitsInput.SelectAll();
+                    textBoxUnitsInput.Focus();
+                }
+            }
+            else
+            {
+                // If range validation fails display the error message below:
+                labelGeneralAverageOutput.Text = "Please enter a whole number.";
+
+                // If validation fails reset focus on the input textbox so user can enter new input.
+                textBoxUnitsInput.SelectAll();
+                textBoxUnitsInput.Focus();
+            }
+
+        }
+    }
+
+}
+
